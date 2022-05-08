1c139200dc492df16bc7555dded76131be8c3390
subochev <caffeine.mgn@gmail.com>
2022-05-08 15:28:28 +0300

    refactoring

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     api("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.6.21")
-    api("pw.binom:kn-clang:0.1.1")
+    api("pw.binom:kn-clang:0.1.2")
     api("io.vertx:vertx-web:4.2.1")
     api("pw.binom:binom-publish:0.1.0")
 }
