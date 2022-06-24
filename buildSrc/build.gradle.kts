302f8855c6a2bf0cc2e3f015ca79475e811d8230
subochev anton <caffeine.mgn@gmail.com>
2022-06-24 21:23:18 +0000

    Kotlin version update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     api("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.6.21")
-    api("pw.binom:kn-clang:0.1.2")
+    api("pw.binom:kn-clang:0.1.3")
     api("io.vertx:vertx-web:4.2.1")
     api("pw.binom:binom-publish:0.1.1")
 }
